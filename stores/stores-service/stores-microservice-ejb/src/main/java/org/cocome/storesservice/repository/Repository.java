7c7599cfe095bb8300ff16feb2a8689a695638d9
Nils Sommer <mail@nilssommer.de>
2017-09-24 17:18:19 +0200

    Add javadoc to repository interface

--- stores/stores-service/stores-microservice-ejb/src/main/java/org/cocome/storesservice/repository/Repository.java
+++ stores/stores-service/stores-microservice-ejb/src/main/java/org/cocome/storesservice/repository/Repository.java
@@ -2,14 +2,49 @@ package org.cocome.storesservice.repository;
 
 import java.util.Collection;
 
+/**
+ * Repository interface for standard CRUD functionality.
+ * 
+ * @author Nils Sommer
+ *
+ * @param <EntityType> Type of the entity
+ * @param <KeyType> Type of the entity's primary key
+ */
 public interface Repository<EntityType, KeyType> {
+	/**
+	 * Create a new instance by persisting it.
+	 * 
+	 * @param entity the entity
+	 * @return the primary key of the entity
+	 */
 	KeyType create(EntityType entity);
 	
+	/**
+	 * Find an entity by its primary key.
+	 * 
+	 * @param id the primary key
+	 * @return the entity
+	 */
 	EntityType find(KeyType id);
 	
+	/**
+	 * Update an entity.
+	 * 
+	 * @param entity the entity
+	 */
 	void update(EntityType entity);
 	
+	/**
+	 * Delete an entity.
+	 * 
+	 * @param entity the entity
+	 */
 	void delete(EntityType entity);
 	
+	/**
+	 * Find all entities of a kind.
+	 * 
+	 * @return the entities
+	 */
 	Collection<EntityType> all();
 }
