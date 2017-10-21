9ae46d2c0e4c70045e1ee1a7156e993c8fd024a7
Nils Sommer <mail@nilssommer.de>
2017-10-21 18:43:45 +0200

    Rename class

--- stores/stores-api-tests/src/test/java/org/cocome/storesapitests/StockItemResourceTests.java
+++ stores/stores-api-tests/src/test/java/org/cocome/storesapitests/StockItemResourceTests.java
@@ -13,7 +13,7 @@ import org.cocome.storesclient.domain.TradingEnterprise;
 import org.junit.Before;
 import org.junit.Test;
 
-public class StockItemTests {
+public class StockItemResourceTests {
 	private StockItemClient client = new StockItemClient();
 	private Store store = new Store();
 	
