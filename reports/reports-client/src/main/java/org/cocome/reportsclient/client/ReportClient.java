389aeeb68c622b418342d0ed96f67c69663357bf
Benkler <niko.benkler@student.kit.edu>
2018-12-06 23:26:31 +0100

    Exception for reportsclient added

--- reports/reports-client/src/main/java/org/cocome/reportsclient/client/ReportClient.java
+++ reports/reports-client/src/main/java/org/cocome/reportsclient/client/ReportClient.java
@@ -1,5 +1,7 @@
 package org.cocome.reportsclient.client;
 
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -8,48 +10,55 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.cocome.reportsclient.config.Config;
+import org.cocome.reportsclient.exceptions.ReportsRestException;
 
 public class ReportClient {
 	private WebTarget webTarget;
-	
+
 	public ReportClient() {
 		Client client = ClientBuilder.newClient();
 		this.webTarget = client.target(Config.getBaseUri());
 	}
-	
-	public String generateEnterpriseDeliveryReport(long enterpriseId) {
-		// JAX-RS seems to not support POST requests without body content,
-		// so we send text/plain with an empty string.
-		Response response = this.webTarget.path("reports")
-				.queryParam("type", "enterprise-delivery")
-				.queryParam("id", enterpriseId)
-				.request()
-				.accept(MediaType.TEXT_HTML_TYPE)
-				.post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
-		return response.readEntity(String.class);
+
+	public String generateEnterpriseDeliveryReport(long enterpriseId) throws ReportsRestException {
+		try {
+			Response response = this.webTarget.path("reports").queryParam("type", "enterprise-delivery")
+					.queryParam("id", enterpriseId).request().accept(MediaType.TEXT_HTML_TYPE)
+					.post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
+			return response.readEntity(String.class);
+		} catch (NotFoundException e) {
+			throw new ReportsRestException(e.getResponse().readEntity(String.class));
+		} catch (ProcessingException e) {
+			throw new ReportsRestException("Connection to reportsservice refused!");
+		}
+
 	}
-	
-	public String generateStoreStockReport(long storeId) {
-		// JAX-RS seems to not support POST requests without body content,
-				// so we send text/plain with an empty string.
-				Response response = this.webTarget.path("reports")
-						.queryParam("type", "stock-report")
-						.queryParam("id", storeId)
-						.request()
-						.accept(MediaType.TEXT_HTML_TYPE)
-						.post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
-				return response.readEntity(String.class);
+
+	public String generateStoreStockReport(long storeId) throws ReportsRestException {
+		try {
+			Response response = this.webTarget.path("reports").queryParam("type", "stock-report")
+					.queryParam("id", storeId).request().accept(MediaType.TEXT_HTML_TYPE)
+					.post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
+			return response.readEntity(String.class);
+		} catch (NotFoundException e) {
+			throw new ReportsRestException(e.getResponse().readEntity(String.class));
+		} catch (ProcessingException e) {
+			throw new ReportsRestException("Connection to reportsservice refused!");
+		}
+
 	}
-	
-	public String generateEnterpriseStockReport(long enterpriseId) {
-		// JAX-RS seems to not support POST requests without body content,
-				// so we send text/plain with an empty string.
-				Response response = this.webTarget.path("reports")
-						.queryParam("type", "enterprise-stock")
-						.queryParam("id", enterpriseId)
-						.request()
-						.accept(MediaType.TEXT_HTML_TYPE)
-						.post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
-				return response.readEntity(String.class);
+
+	public String generateEnterpriseStockReport(long enterpriseId) throws ReportsRestException {
+		try {
+			Response response = this.webTarget.path("reports").queryParam("type", "enterprise-stock")
+					.queryParam("id", enterpriseId).request().accept(MediaType.TEXT_HTML_TYPE)
+					.post(Entity.entity("", MediaType.TEXT_PLAIN_TYPE));
+			return response.readEntity(String.class);
+		} catch (NotFoundException e) {
+			throw new ReportsRestException(e.getResponse().readEntity(String.class));
+		} catch (ProcessingException e) {
+			throw new ReportsRestException("Connection to reportsservice refused!");
+		}
+
 	}
 }
---
+++ reports/reports-client/src/main/java/org/cocome/reportsclient/exceptions/ReportsRestException.java
@@ -0,0 +1,14 @@
+package org.cocome.reportsclient.exceptions;
+
+public class ReportsRestException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2870278230312852454L;
+	
+	public ReportsRestException(String message) {
+		super(message);
+	}
+
+}
